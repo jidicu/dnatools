c364343dbbbaef399a2a2c5925dd2349bfc61ca6
jidicu <johanan.idicula@mail.mcgill.ca>
2016-05-20 20:01:44 -0400

    oligo.py now accepts user input

--- oligo.py
+++ oligo.py
@@ -1,14 +1,19 @@
 # This is a repeating sequence. The length of the repeat here is 50 bp.
+repeat_len = raw_input("How long do you want your repeat to be? ")
 
-for i in range (0,50):
+oligo = ""
+
+for i in range (0,int(repeat_len)-1):
     oligo = oligo + "A"
 
+sticky = raw_input("What is the sticky end sequence? ")
+
 # This is your sticky end.
-oligo = oligo + "CCGGTTGGCC"
+oligo = oligo + sticky
 
 #This prints your oligo as an output.
 print(oligo)
-print(len(oligo))
+print(str(len(oligo)) + " bp")
 
 # This also writes your oligo into a text file.
 f = open('oligo.txt','w')

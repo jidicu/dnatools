49d80fc883bb03e1ee462f80f3fc41c86be4cd56
jidicu <johanan.idicula@mail.mcgill.ca>
2016-05-20 22:00:01 -0400

    Returns list of codons.

--- aa_seq/aaseq.py
+++ aa_seq/aaseq.py
@@ -1,6 +1,6 @@
 import re
 
-class AminoAcid
+class AminoAcid:
     abbrev = ""
     codon = ""
     name = ""
@@ -12,12 +12,17 @@ class AminoAcid
 
 # TODO Add polarity, charge, etc
 
-dna_seq = raw_input("Enter your sequence.")
+def codons(rna_seq): # This method splits an input into a list of codons.
+    return [ rna_seq[start:start+3] for start in range(0, len(rna_seq), 3) ]
+
+dna_seq = raw_input("Enter your sequence: ")
 allowed_char = re.compile(r"a|c|t|g|u|A|T|C|G|U")
 if not allowed_char.findall(dna_seq):
-    print("You did not enter a valid sequence.")
+    print("You did not enter a valid sequence. Restart the program and try again.")
+    raise SystemExit
+if len(dna_seq)%3 != 0:
+    print("You did not enter a sequence consisting only of codons. Restart the program and try again.")
+    raise SystemExit
 dna_seq = dna_seq.upper()
 rna_seq = dna_seq.replace("T","U")
-
-def codons(rna_seq, 3):
-    return [ rna_seq[start:start+3] for start in range(0, len(rna_seq), 3) ]
+print(codons(rna_seq))
